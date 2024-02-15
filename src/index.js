8e1748b3d81bd096192803d2987a904e63da000e
DykFlint <konstantin.tamoev@gmail.com>
2024-02-15 11:54:00 +0100

    Add header image to workflow

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import createHomePage from "./home.js";
 import createMenu from "./menu.js";
+import LynnsTable from './images/lynn.svg';
 
 const homeButton = document.querySelector('#home-button');
 const menuButton = document.querySelector('#menu-button');
-
+const headerImageDiv = document.querySelector('#header-image');
+// add header image
+headerImageDiv.setAttribute('id', 'header-image');
+const headerImage = new Image();
+headerImage.src = LynnsTable;
+headerImageDiv.appendChild(headerImage);
 homeButton.addEventListener("click", createHomePage);
 menuButton.addEventListener("click", createMenu);
 // default page is home page
